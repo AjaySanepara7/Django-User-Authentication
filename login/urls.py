2ba40c3a9373caac38f0b55e8c8b3aa525cb9456
AjaySanepara7 <ajaysanepara7@gmail.com>
2024-12-02 17:46:55 +0530

    fix: update:urls.py

--- login/urls.py
+++ login/urls.py
@@ -7,12 +7,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("login_page/", views.login_page, name="login_page"),
     path("successful_login/", views.successful_login, name="successful_login"),
-    path("failed_login/", views.failed_login, name="failed_login"),
     path("register/", views.registration, name="registration"),
-    path("registration_successful/", views.registration_successful, name="registration_successful"),
-    path("logout_page/", views.logout_page, name="logout_page"),
     path("forget_password_page/", views.forget_password_page, name="forget_password_page"),
-    path("success_change_password/", views.success_change_password, name="success_change_password"),
-    path("fail_change_password/", views.fail_change_password, name="fail_change_password"),
-    path("user_already_exists/", views.user_already_exists, name="user_already_exists")
 ]
