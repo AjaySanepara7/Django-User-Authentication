d52abfd1be055695f8bcf46069c10570f2624fda
AjaySanepara7 <ajaysanepara7@gmail.com>
2024-12-06 11:58:38 +0530

    fix: change urls for class based views

--- login/urls.py
+++ login/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from login import views
+from django.contrib.auth.decorators import login_required
 
 
 app_name = "login"
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("login_page/", views.login_page, name="login_page"),
-    path("successful_login/", views.successful_login, name="successful_login"),
-    path("register/", views.registration, name="registration"),
-    path("forget_password_page/", views.forget_password_page, name="forget_password_page"),
+    path("", views.Home.as_view(), name="home"),
+    path("login_page/", views.LoginPageView.as_view(), name="login_page"),
+    path("successful_login/", login_required(views.SuccessfulLoginView.as_view()), name="successful_login"),
+    path("register/", views.Registration.as_view(), name="registration"),
+    path("forget_password_page/", views.ForgetPasswordView.as_view(), name="forget_password_page"),
 ]
